331942816320297387cf9d9b534ef6eab60b71aa
Yuki Kobayashi <kobayashi.yuki@gmail.com>
2023-12-17 16:24:43 +0900

    Use goodvibes

--- radio.sh
+++ radio.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 export XDG_RUNTIME_DIR=/run/user/1000
 
+#whoami > /home/pi/work/radio/whoami.log
+#echo $@ >> /home/pi/work/radio/whoami.log
+#env >> /home/pi/work/radio/whoami.log
+
 TARGETID= # empty by default
 while [ $# -ge 1 ]; do
   TARGETID=$1
   shift
 done
 
-CHK=`ps -ef | grep mpg123 | grep -v grep`
-RETVAL=$? # 1: not found, 0: found
 RADIOIDFILE=/var/tmp/radioid.txt
-RADIOINFOFILE=/var/tmp/radioinfo.txt
 RADIOID=1
+# VRT URLs: https://www.vrt.be/nl/aanbod/kijk-en-luister/radio-luisteren/streamingslinks-radio/
 RADIOURL[0]="http://icecast.vrtcdn.be/klaracontinuo-high.mp3"
 RADIOURL[1]="http://icecast.vrtcdn.be/stubru_bruut-high.mp3"
 RADIOURL[2]="http://icecast.vrtcdn.be/stubru-high.mp3"
@@ -20,6 +22,14 @@ RADIOURL[4]="http://icecast.vrtcdn.be/mnm-high.mp3"
 RADIOURL[5]="/home/pi/Music/Wave.list"
 RADIOURL[6]="http://live-radio01.mediahubaustralia.com/JAZW/mp3/"
 RADIOIDMAX=7
+
+USE_MPG123=0
+if [ $USE_MPG123 -eq 1 ]; then
+  CHK=`ps -ef | grep mpg123 | grep -v grep`
+else
+  CHK=`ps -ef | grep goodvibes | grep -v grep`
+fi
+RETVAL=$? # 1: not found, 0: found
 
 echo RETVAL=$RETVAL
 
@@ -35,21 +45,25 @@ if [ $RETVAL -eq 1 ]; then
   echo RADIOID=$RADIOID
   TARGETURL=${RADIOURL[$RADIOID]}
   echo $TARGETURL
-  if [ $? -eq 0 ]; then
-    # Bluetooth Speaker Connected
-    BTDEVICE="-a bluealsa:DEV=6C:5A:B5:70:F8:2A"
-  else
-    BTDEVICE=""
-  fi
+
+  BTDEVICE="-a bluealsa:DEV=6C:5A:B5:70:F8:2A"
+
   VOLUME=5
   if [ ${TARGETURL:0:4} == "http" ]; then
-    curl -L $TARGETURL 2> /dev/null | /usr/bin/mpg123 --reopen --gain $VOLUME $BTDEVICE - 2> $RADIOINFOFILE & 
+    if [ $USE_MPG123 -eq 1 ]; then
+      curl -L $TARGETURL 2> /dev/null | /usr/bin/mpg123 --reopen --gain $VOLUME $BTDEVICE - 2> $RADIOINFOFILE & 
+    else
+      goodvibes --without-ui $TARGETURL &
+    fi
+    echo "started"
   else
     echo FILE
     echo /usr/bin/mpg123 $TARGETURL $BTDEVICE 
     /usr/bin/mpg123 --list $TARGETURL $BTDEVICE  -
   fi
   echo $RADIOID > $RADIOIDFILE
+  exit
+
   if [ "$BTDEVICE" != "" ]; then
     sleep 15
     # check
